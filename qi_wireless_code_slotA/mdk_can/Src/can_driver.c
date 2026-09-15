56155eea119930e35ece21246ed575b77bc6b17c
young-nights <2863692760@qq.com>
2026-09-15 15:57:47 +0800

    fix(can_driver): 采样点改为75%
    BTS1=63,BTS2=9(87.5%) → BTS1=54,BTS2=18(75%)
    18MHz / 72Tq = 250kbps, SP = 54/72 = 75%
    
    修改: slotA/slotB/bootloader 三个 can_driver.c

--- qi_wireless_code_slotA/mdk_can/Src/can_driver.c
+++ qi_wireless_code_slotA/mdk_can/Src/can_driver.c
@@ -42,8 +42,8 @@
  */
 #define CAN_BITTIME_DIV                 10U
 #define CAN_BITTIME_SJW                 4U
-#define CAN_BITTIME_BTS1                63U
-#define CAN_BITTIME_BTS2                 9U
+#define CAN_BITTIME_BTS1                54U
+#define CAN_BITTIME_BTS2                18U
 
 /* private variables ---------------------------------------------------------*/
 
